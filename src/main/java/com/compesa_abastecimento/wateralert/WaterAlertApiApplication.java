1a5d52f01c5906f1a0d7ca16cb855f8b70d20c58
Cleyton Melo <cleytonw11@gmail.com>
2025-10-02 16:39:45 -0300

    first commit

---
+++ src/main/java/com/compesa_abastecimento/wateralert/WaterAlertApiApplication.java
@@ -0,0 +1,13 @@
+package com.compesa_abastecimento.wateralert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaterAlertApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaterAlertApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/compesa_abastecimento/wateralert/WaterAlertApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.compesa_abastecimento.wateralert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaterAlertApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
